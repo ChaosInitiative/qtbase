f291b24ca6f0854ab6114384cd2cd220dfd0b23a
SCell555 <kubci.rusnk645@gmail.com>
2022-06-08 22:43:20 +0200

    Update CI

---
+++ cmake/clang.toolchain.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR AMD64)
+
+set(CMAKE_C_COMPILER /usr/local/bin/clang-cl)
+set(CMAKE_CXX_COMPILER /usr/local/bin/clang-cl)
+set(CMAKE_C_FLAGS "-Wno-ignored-pragmas")
+set(CMAKE_CXX_FLAGS "-Wno-ignored-pragmas")
